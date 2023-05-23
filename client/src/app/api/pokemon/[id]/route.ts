a91b2b4041efced7093dec64ab70343d170ec350
Bosch <boschio91@gmail.com>
2023-05-23 18:53:22 -0400

    Implemented apiclient for handling frontend to backend requests.

--- client/src/app/api/pokemon/[id]/route.ts
+++ client/src/app/api/pokemon/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export async function GET(request: Request, { params }) {
+export async function GET(request: Request, { params }: any) {
     const id = params.id
 
     return NextResponse.json(`https://pokeapi.co/api/v2/pokemon/${id}/`)
